755faf41825d7cfe9aca6e8dccfa3cc7a8932820
Your Name <you@example.com>
2018-11-10 16:08:21 +0100

    redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { provider } from 'react-redux';
-import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk'
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 
-const store = createStore(searchRobots)
+const logger = createLogger();
 
-ReactDOM.render(<App store={store} />,document.getElementById('root'));
+const rootsReducer = combineReducers({ searchRobots, requestRobots})
+const store = 
+    createStore(rootsReducer, applyMiddleware(thunkMiddleware, logger))
+
+ReactDOM.render(
+                 <Provider store={store}>
+                    <App />
+                 </Provider>, document.getElementById('root'));
 registerServiceWorker();
